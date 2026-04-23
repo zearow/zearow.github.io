fc6ea1858cd593e538951aa58d1e0d107520d06f
Richárd Ládi <richard.ladi@icloud.com>
2026-04-23 08:42:20 +0200

    Add SVG, PNG, and ICO favicons with Apple & Android PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,8 +13,12 @@ export default defineNuxtConfig({
       htmlAttrs: { lang: 'en' },
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
+        { rel: 'manifest', href: '/site.webmanifest' }
       ],
       meta: [
         { name: 'apple-mobile-web-app-title', content: 'zearøw' },
